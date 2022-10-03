f766bee49ab4e0ece9817afe616c2eb1bb1d8ad8
HilalVildan <hilalsarioglu96@gmail.com>
2022-10-03 15:38:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+fetch("./tv-shows.json")
+.then((response) => response.json())
+.then((data)=> {
+    const ekranaBastir = (data) => {
+
+        const shows = document.querySelector(".shows")
+
+        data.forEach((a) => {
+
+            shows.innerHTML += `<div class="card col-4 m-2" style="width: 18rem;">
+  <img src="${a.show.image.medium}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${a.show.name}</h5>
+    <a href="${a.show.url}" class="btn btn-primary">Details</a>
+  </div>
+</div> `;
+            
+        });
+    }
+
+    ekranaBastir(data)
+});
+
